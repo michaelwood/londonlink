9c6434e07810fd6988892913eeb4ffc1481ec67e
Michael Wood <internet@michaelwood.me.uk>
2017-10-26 16:52:44 +0100

    Delete data When deleting the event also delete the page it created

--- delete-data.php
+++ delete-data.php
@@ -4,18 +4,25 @@ function delete_data ()
 {
   llg_db_connection ();
 
-  $event = $_POST['event_name_selected'];
+  $event_name = mysql_real_escape_string($_POST['event_name_selected']);
 
-  if (!isset ($event)) {
+  if (!isset ($event_name)) {
     return;
   }
 
 
-  $sql = 'DELETE FROM bookings WHERE event_name="'.mysql_real_escape_string ($event).'"';
+  $res = mysql_query('SELECT wp_page_id FROM event WHERE name="'.$event_name.'"') or die (mysql_error());
+
+  $wp_page_id = mysql_result ($res, 0);
+
+  wp_delete_post($wp_page_id, false);
+
+
+  $sql = 'DELETE FROM bookings WHERE event_name="'.$event_name.'"';
 
   mysql_query ($sql) or die (mysql_error ());
 
-  $sql = 'DELETE FROM event WHERE name="'.mysql_real_escape_string ($event).'"';
+  $sql = 'DELETE FROM event WHERE name="'.$event_name.'"';
 
   mysql_query ($sql) or die (mysql_error ());
 
