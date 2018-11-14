8a3b7f716bbf22cf39f065c724600b253bbb14b3
Michael Wood <internet@michaelwood.me.uk>
2018-11-14 18:34:36 +0000

    widget: Add the number of days until the event

--- widget.php
+++ widget.php
@@ -9,6 +9,9 @@ class NextEventWidget extends WP_Widget {
 
   //function can take $args, $instance
   function widget($args, $instance) {
+
+    $today = date_create("now");
+
     $db = llg_db_connection();
 
      $sql = "SELECT `name`, `wp_page_id`, `event_start_date`, STR_TO_DATE(event_start_date, '%d/%m/%y') as 'realDate'  FROM `events` WHERE `enabled`=1 ORDER BY `realDate` ASC";
@@ -26,17 +29,23 @@ class NextEventWidget extends WP_Widget {
            <ul>');
 
      while($event_data = mysqli_fetch_assoc ($result)){
+       $event_date = date_create_from_format('d/m/Y', $event_data['event_start_date']);
+
        $page_link = get_permalink($event_data['wp_page_id']);
-       echo('<li><a href="'.$page_link.'">');
+       echo('<li><a href="'.$page_link.'" >');
        echo($event_data['event_start_date'].' - '.$event_data['name']);
+
+       if ($event_date){
+         $diff = date_diff($today, $event_date);
+         echo (' <span style="font-size: 0.8em">(in just '.$diff->format('%a days!').')</span> ');
+       }
+
        echo('</a></li>');
      }
 
      echo('</ul>
          </div>
        </div>');
-
-
   }
   function update( $new_instance, $old_instance ) {
   // Save widget options
