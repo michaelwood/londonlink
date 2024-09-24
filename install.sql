3ef38489d4062722c18f1f04a0b1a96dcf41378f
Michael Wood <internet@michaelwood.me.uk>
2024-09-24 17:53:32 +0100

    install.sql: Update to latest schema

--- install.sql
+++ install.sql
@@ -1,18 +1,36 @@
+-- Generation Time: Sep 24, 2024 at 04:48 PM
+-- Server version: 10.11.6-MariaDB-0+deb12u1
+-- PHP Version: 8.2.15
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
 
 CREATE TABLE `bookings` (
   `id` int(11) NOT NULL,
   `event_id` int(11) NOT NULL,
-  `submit_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `data` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `submit_timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `data` text NOT NULL,
+  `admin_notes` text DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
 
 CREATE TABLE `events` (
   `id` int(11) NOT NULL,
@@ -22,26 +40,68 @@ CREATE TABLE `events` (
   `event_start_date` text NOT NULL,
   `event_end_date` text NOT NULL,
   `password` text NOT NULL,
-  `enabled` tinyint(1) NOT NULL DEFAULT '1',
-  `cost` float DEFAULT '0',
+  `enabled` tinyint(1) NOT NULL DEFAULT 1,
+  `cost` float DEFAULT 0,
   `wp_page_id` int(11) DEFAULT NULL,
-  `form_template` text,
-  `listed` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `listed` tinyint(1) NOT NULL DEFAULT 1,
+  `form_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Table structure for table `forms`
+--
 
+CREATE TABLE `forms` (
+  `id` int(11) NOT NULL,
+  `template` text NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookings`
+--
 ALTER TABLE `bookings`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `events`
+--
 ALTER TABLE `events`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `forms`
+--
+ALTER TABLE `forms`
+  ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=362;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1019;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `forms`
+--
+ALTER TABLE `forms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-ALTER TABLE `bookings` ADD `admin_notes` TEXT NULL DEFAULT NULL AFTER `data`;
